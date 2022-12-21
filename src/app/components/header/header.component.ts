ac6451741c1471ba641849f176549f2e8bfe6c53
shimy8617 <cristinashim@yahoo.com.ar>
2022-12-21 16:23:16 -0300

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faInstagram, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  faFacebook = faFacebook;
+  faLinkedin = faLinkedin;
+  faInstagram = faInstagram;
   title = 'My Portfolio';
   
   constructor() {}
