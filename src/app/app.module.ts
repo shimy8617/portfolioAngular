cee9991cc3e4c17b94618b5c03ae5db85a8eb957
shimy8617 <cristinashim@yahoo.com.ar>
2022-12-22 11:11:40 -0300

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { SoftSkillsComponent } from './components/soft-skills/soft-skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    ProfileComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SoftSkillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
